747ff60b3cc26f368d42683b53d878502523f327
akcode7 <ashishgotapogo10@gmail.com>
2025-07-12 15:00:39 +0530

    dashboard > added losting button

--- src/components/userDashboardInfo.jsx
+++ src/components/userDashboardInfo.jsx
@@ -164,6 +164,11 @@ function UserDashboardInfo() {
                 >
                   Update Info
                 </button>
+                <button
+                  className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+                >
+                  Add Product
+                </button>
               </div>
             </form>
           </div>
